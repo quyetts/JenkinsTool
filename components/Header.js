49d9e01fe733018180950444e64aebd4d5b82555
quyetts <quyetts@gmail.com>
2022-03-23 16:15:34 +0700

    fix typo

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
         <div className="container xl:max-w-screen-xl mx-auto flex justify-between">
           <Logo />
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            {(session?.user?.email === quyetts@gmail.com"" || session?.user?.email === "hoangntvn@gmail.com") ?
+            {(session?.user?.email === "quyetts@gmail.com" || session?.user?.email === "hoangntvn@gmail.com") ?
               <Link href="/admin">
                 <a className="mr-5 hover:text-red-900">
                   Admin
